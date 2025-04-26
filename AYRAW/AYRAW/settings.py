7ff06d1a624343f2cfd79f7c053cfefcf2fbb373
mahendra-bhoi <mahendrabhoi.72@gmail.com>
2025-04-26 19:18:43 +0530

    email functionalites setting

--- AYRAW/AYRAW/settings.py
+++ AYRAW/AYRAW/settings.py
@@ -134,3 +134,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mahendra1bhoi@gmail.com'
+EMAIL_HOST_PASSWORD = 'ceqc uuxo uvfq cnvq'
+DEFAULT_FROM_EMAIL = 'AYRA <mahendra1bhoi@gmail.com>'
